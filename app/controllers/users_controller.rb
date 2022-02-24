024271bf928b48e9d8f1d4819109f852bbd65757
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-24 17:48:48 +0000

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,13 +24,14 @@ class UsersController < ApplicationController
   end
   
   def update
-    user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
-    the_user = matching_users.at(0)
+    the_username = params.fetch("username")
+
+    matching_users = User.where({ :username => the_username })
+    @user = matching_users.at(0)
     
-    the_user.username = params.fetch("query_username")
-    the_user.save
-    redirect_to("/users/#{user.username}")
+    @user.username = params.fetch("query_username")
+    @user.save
+    redirect_to("/users/" + @user.username)
   end
 
 end
